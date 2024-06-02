9ed990e5d7d6d9a4db4142b9b63823ab377e3e57
jprfct@protonmail.com <jprfct@protonmail.com>
2024-06-02 14:40:16 -0700

    added popular tv shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
   results.forEach((movie) => {
@@ -35,16 +36,54 @@ async function displayPopularMovies() {
   });
 }
 
+// Display 20 most popular tv shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+    `;
+
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
 // Fetch data from TMDB
 async function fetchAPIData(endpoint) {
   const API_KEY = "470c812dfa0310c122622dbc37aeef65";
   const API_URL = "https://api.themoviedb.org/3/";
+
+  showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
+
+  hideSpinner();
 
   return data;
 }
@@ -59,6 +98,14 @@ function highLightActiveLink() {
   });
 }
 
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
 // console.log(global.currentPage);
 
 // Init App
@@ -69,7 +116,7 @@ function init() {
       break;
 
     case "/shows.html":
-      console.log("Shows");
+      displayPopularShows();
       break;
 
     case "/movie-details.html":
