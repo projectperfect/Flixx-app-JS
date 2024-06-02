78b488a51d7926559fe70bd73accace686975681
jprfct@protonmail.com <jprfct@protonmail.com>
2024-06-02 15:59:23 -0700

    added the swiper api to the home page

--- js/script.js
+++ js/script.js
@@ -225,6 +225,51 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Display slider movies
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+          </div>
+    `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = "470c812dfa0310c122622dbc37aeef65";
@@ -271,6 +316,7 @@ function hideSpinner() {
 function init() {
   switch (global.currentPage) {
     case "/index.html":
+      displaySlider();
       displayPopularMovies();
       break;
 
